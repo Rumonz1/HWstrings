fb22fbb097b669d1fc15eca28d365dbc7499de91
Rumonz <lone_assassin@mail.ru>
2022-11-18 17:53:16 +0300

    Выполнил задание 3 урока Строки

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         //Задание 1
-        String firstName = "Ivan";
-        String secondName = "Ivanov";
-        String lastName = "Ivanovich";
+        String firstName = "Семён";
+        String secondName = "Иванов";
+        String lastName = "Семёнович";
         String fullName = secondName+" "+firstName+" "+lastName;
+        fullName = fullName.replace("ё","е");
         System.out.println("ФИО сотрудника- "+fullName);
         //Задание 2
         String nameBig = fullName.toUpperCase();
         System.out.println("Данные ФИО сотрудника для заполнения отчёта - "+nameBig);
+        //Задание 3
     }
 }
